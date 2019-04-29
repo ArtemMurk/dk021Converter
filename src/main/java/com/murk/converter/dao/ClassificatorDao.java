0f9ef469488d30dfe658c47484074280bc4b5688
win1 <muravlevars@gmail.com>
2019-04-29 19:30:35 +0300

    Add Classificator Dao level

---
+++ src/main/java/com/murk/converter/dao/ClassificatorDao.java
@@ -0,0 +1,66 @@
+package com.murk.converter.dao;
+
+import com.murk.converter.model.Classificator;
+import com.murk.converter.utils.PropertiesReader;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Set;
+
+import static com.murk.converter.dao.SqlHelper.INSERT_CLASSIFICATOR;
+
+public class ClassificatorDao {
+
+    private String url = PropertiesReader.readProperty("./help/db.properties","db_connection");
+    private Properties props = new Properties();
+
+    {
+        props.setProperty("user", PropertiesReader.readProperty("./help/db.properties","login"));
+        props.setProperty("password", PropertiesReader.readProperty("./help/db.properties","password"));
+
+    }
+
+    public void save(Set<Classificator> classificators)
+    {
+        try(Connection connection = DriverManager.getConnection(url,props))
+        {
+            connection.setAutoCommit(false);
+            classificators.forEach(classificator ->
+            {
+                save(classificator,connection);
+                try {
+                    connection.commit();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void save(Classificator classificator, Connection connection)
+    {
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_CLASSIFICATOR);)
+        {
+            statement.setInt(1,classificator.getId());
+            statement.setShort(2,classificator.getNum());
+
+            if (classificator.getParentId()== null)
+            {
+                statement.setNull(3, Types.SMALLINT);
+            } else
+                {
+                    statement.setInt(3,classificator.getParentId());
+                }
+
+            statement.setString(4,classificator.getName());
+
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/com/murk/converter/dao/SqlHelper.java
@@ -0,0 +1,5 @@
+package com.murk.converter.dao;
+
+public class SqlHelper {
+    public static String INSERT_CLASSIFICATOR = "INSERT INTO dk_021 VALUES (?,?,?,?);";
+}
